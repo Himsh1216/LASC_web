3f6ac24f08f5e9e61d8d4c447db9c539f978f612
Himsh1216 <140539171+Himsh1216@users.noreply.github.com>
2024-12-02 10:23:41 +0530

    Update ServicesPage.jsx

--- src/components/pages/ServicesPage.jsx
+++ src/components/pages/ServicesPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MicroscopeIcon, Wrench, GraduationCap, X } from 'lucide-react';
+import { MicroscopeIcon, Wrench, GraduationCap } from 'lucide-react';
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card.tsx';
 import {
   Dialog,
